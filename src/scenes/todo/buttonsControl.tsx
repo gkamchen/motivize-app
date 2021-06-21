c14e9583c4bd2f77dceb4e89c80bce71621f4d8f
wagner <wagnercamara1994@gmail.com>
2021-06-21 20:40:57 -0300

    Async Storage

--- src/scenes/todo/buttonsControl.tsx
+++ src/scenes/todo/buttonsControl.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, 
-          View,
-          Platform,
-          PermissionsAndroid,
-          Alert,
-        } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Platform,
+  PermissionsAndroid,
+  Alert,
+} from 'react-native';
 import {
   Layout,
   Text,
@@ -14,7 +15,7 @@ import {
   Card,
 } from '@ui-kitten/components';
 import Geolocation from '@react-native-community/geolocation';
-import { Console } from 'console';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const StarIcon = (props) => (
   <Icon {...props} name='star' />
@@ -26,7 +27,7 @@ const LoadingIndicator = (props) => (
   </View>
 );
 
-const INI_DIA = 'INICIAR DIA'; 
+const INI_DIA = 'INICIAR DIA';
 const FIM_DIA = 'FINALIZAR DIA';
 
 const INI_INTERVALO = 'INICIAR INTERVALO';
@@ -35,8 +36,8 @@ const FIM_INTERVALO = 'FINALIZAR INTERVALO';
 const INI_ATENDIMENTO = 'INICIAR ATENDIMENTO';
 const FIM_ATENDIMENTO = 'FINALIZAR ATENDIMENTO';
 
-const INI_STATUS='primary';
-const FIM_STATUS='danger';
+const INI_STATUS = 'primary';
+const FIM_STATUS = 'danger';
 
 const APAR_INI = 'filled';
 const APAR_FIM = 'outline';
@@ -45,8 +46,8 @@ const STOP = 'stop';
 const START = 'start';
 const CLEAR = 'clear';
 
-const DISABLE_BOTTON=true;
-const ENABLE_BOTTON=false;
+const DISABLE_BOTTON = true;
+const ENABLE_BOTTON = false;
 
 var ATIVAR_BDIA = ENABLE_BOTTON;
 var ATIVAR_BATENDIMENTO = DISABLE_BOTTON;
@@ -54,7 +55,7 @@ var ATIVAR_BINTERVALO = DISABLE_BOTTON;
 
 var INTERVALO = INI_INTERVALO;
 var INTERVALO_STATUS = INI_STATUS;
-var INTERVALO_APAR= APAR_INI;
+var INTERVALO_APAR = APAR_INI;
 
 var DIA = INI_DIA;
 var DIA_STATUS = INI_STATUS;
@@ -62,103 +63,126 @@ var DIA_APAR = APAR_INI;
 
 var ATENDIMENTO = INI_ATENDIMENTO;
 var ATENDIMENTO_STATUS = INI_STATUS;
-var ATENDIMENTO_APAR= APAR_INI;
+var ATENDIMENTO_APAR = APAR_INI;
 
 var cronometroDia;
 
-function returnDateTime(){
+function returnDateTime() {
 
   console.log("\n LOG: -----------> DATA E HORA -----> ");
 
-var data = new Date();
+  var data = new Date();
 
-var dia     = data.getDate();           // 1-31
-var mes     = data.getMonth();          // 0-11 (zero=janeiro)
-var ano    = data.getFullYear();       // 4 dígitos
-var hora    = data.getHours();          // 0-23
-var min     = data.getMinutes();        // 0-59
-var seg     = data.getSeconds();        // 0-59
+  var dia = data.getDate();           // 1-31
+  var mes = data.getMonth();          // 0-11 (zero=janeiro)
+  var ano = data.getFullYear();       // 4 dígitos
+  var hora = data.getHours();          // 0-23
+  var min = data.getMinutes();        // 0-59
+  var seg = data.getSeconds();        // 0-59
 
-var dataF=(dia+"/"+mes+"/"+ano)
-var horario = (hora +":"+ min +":"+ seg);
+  var dataF = (dia + "/" + mes + "/" + ano)
+  var horario = (hora + ":" + min + ":" + seg);
 
-return {dataF,horario};
+  return { dataF, horario };
 
 };
 
 
 export const ButtonsControl = () => {
-  
+
   //ObterStateDay();
 
-  const [currentLatitude,setCurrentLatitude] = React.useState('');
-  const [currentLogitude,setCurrentLongitude] = React.useState('');
-  const [watchId,setWatchId] = React.useState(0);
+  const [currentLatitude, setCurrentLatitude] = React.useState(' ');
+  const [currentLogitude, setCurrentLongitude] = React.useState(' ');
+  const [watchId, setWatchId] = React.useState(0);
 
-  const [currentDay,setDay] = React.useState('');
-  const [currentUser,setUser] = React.useState('Wagner Teste');
-  const [currentHorTrab,setHorTrab] = React.useState('0.0');
-  const [currentHorAten,setHorAten] = React.useState('0.0');
-  const [currentHorDesl,setHorDesl] = React.useState('0.0');
-  
-  const [currentIniDia,setIniDia] = React.useState('');
-  const [currentFimDia,setFimDia] = React.useState('');
-  const [currentIniInt,setIniInt] = React.useState('');
-  const [currentFimInt,setFimInt] = React.useState('');
+  const [currentDay, setDay] = React.useState(' ');
+  const [currentUser, setUser] = React.useState('Wagner Teste');
+  const [currentHorTrab, setHorTrab] = React.useState('0.0');
+  const [currentHorAten, setHorAten] = React.useState('0.0');
+  const [currentHorDesl, setHorDesl] = React.useState('0.0');
+
+  const [currentIniDia, setIniDia] = React.useState(' ');
+  const [currentFimDia, setFimDia] = React.useState(' ');
+  const [currentIniInt, setIniInt] = React.useState(' ');
+  const [currentFimInt, setFimInt] = React.useState(' ');
 
   const [bDia, setDia] = React.useState(INI_DIA);
   const [bAtendimento, setAtendimento] = React.useState(INI_ATENDIMENTO);
   const [bIntervalor, setIntervalo] = React.useState(INI_INTERVALO);
 
-  const [currentCroDia,setCroDia] = React.useState('');
+  const [currentCroDia, setCroDia] = React.useState('');
 
   const cronHorTrab = () => {
 
   };
 
   const cronHorAten = () => {
-    
+
   };
 
   const cronHorDesl = () => {
-    
+
+  };
+
+  const salveStateDia = () => {
+
+      gravar('latitude',currentLatitude);
+      gravar('longitude',currentLogitude);
+      gravar('inicioDia',currentIniDia);
+      gravar('fimDia',currentFimDia);
+
+    buscar('latitude');
+    buscar('longitude');
+    buscar('inicioDia');
+    buscar('fimDia');
+
+  };
+
+  const gravar = (chave:string,valor:any) => {
+    AsyncStorage.setItem(chave,valor);
+  }
+
+  const buscar = async (chave:string) =>{
+    const valor = await AsyncStorage.getItem(chave);
+    console.log(valor);
   };
 
   const getLocation = () => {
 
-      Geolocation.getCurrentPosition(
-        (position) => {
-          const currentLatitude = JSON.stringify(position.coords.latitude);
-          const currentLogitude = JSON.stringify(position.coords.longitude);
-          setCurrentLatitude(currentLatitude);
-          setCurrentLongitude(currentLogitude);
-        },
-        (error) => console.log(error.message),{
-          enableHighAccuracy:true, timeout: 20000, maximumAge: 1000
-        }
-      );
-      const watchId = Geolocation.watchPosition((position) =>{
+    Geolocation.getCurrentPosition(
+      (position) => {
         const currentLatitude = JSON.stringify(position.coords.latitude);
         const currentLogitude = JSON.stringify(position.coords.longitude);
         setCurrentLatitude(currentLatitude);
         setCurrentLongitude(currentLogitude);
-      });
-      setWatchId(watchId);
+      },
+      (error) => console.log(error.message), {
+      enableHighAccuracy: true, timeout: 20000, maximumAge: 1000
+    }
+    );
+    const watchId = Geolocation.watchPosition((position) => {
+      const currentLatitude = JSON.stringify(position.coords.latitude);
+      const currentLogitude = JSON.stringify(position.coords.longitude);
+      setCurrentLatitude(currentLatitude);
+      setCurrentLongitude(currentLogitude);
+    });
+    setWatchId(watchId);
   }
 
-  const claerLocation = () =>{
+  const claerLocation = () => {
     Geolocation.clearWatch(watchId);
   }
 
   const onIntervalo = () => {
 
-    console.log("\nLOG: ----------->CLICK  BOTÃO INTERVALO -------> "+INTERVALO);
+    console.log("\nLOG: ----------->CLICK  BOTÃO INTERVALO -------> " + INTERVALO);
 
-    if (INTERVALO === INI_INTERVALO){
+    if (INTERVALO === INI_INTERVALO) {
 
       setIniInt(returnDateTime().horario);
       setFimInt("");
-      
+
       INTERVALO = FIM_INTERVALO;
       INTERVALO_STATUS = FIM_STATUS;
       INTERVALO_APAR = APAR_FIM;
@@ -167,7 +191,7 @@ export const ButtonsControl = () => {
       ATIVAR_BATENDIMENTO = DISABLE_BOTTON;
       ATIVAR_BINTERVALO = ENABLE_BOTTON;
 
-    }else{
+    } else {
 
       setFimInt(returnDateTime().horario);
 
@@ -175,21 +199,21 @@ export const ButtonsControl = () => {
       ATIVAR_BATENDIMENTO = ENABLE_BOTTON;
       ATIVAR_BINTERVALO = ENABLE_BOTTON;
 
-      INTERVALO = INI_INTERVALO;  
+      INTERVALO = INI_INTERVALO;
       INTERVALO_STATUS = INI_STATUS;
       INTERVALO_APAR = APAR_INI;
 
     }
-    
+
     setIntervalo(INTERVALO);
-    
+
   };
 
   const onDia = async () => {
 
     console.log("\nLOG: -----------> CLICK BOTÃO DIA -----> " + DIA);
 
-    if (DIA === INI_DIA){
+    if (DIA === INI_DIA) {
 
       getLocation();
 
@@ -205,28 +229,34 @@ export const ButtonsControl = () => {
       ATIVAR_BATENDIMENTO = ENABLE_BOTTON;
       ATIVAR_BINTERVALO = ENABLE_BOTTON;
 
-    }else{
+    } else {
 
       setFimDia(returnDateTime().horario);
+
+      console.log(currentFimDia);
+
+      salveStateDia();
 
       ATIVAR_BDIA = ENABLE_BOTTON;
       ATIVAR_BATENDIMENTO = DISABLE_BOTTON;
       ATIVAR_BINTERVALO = DISABLE_BOTTON;
 
-      DIA = INI_DIA;  
+      DIA = INI_DIA;
       DIA_STATUS = INI_STATUS;
       DIA_APAR = APAR_INI;
 
+      claerLocation();
+
     }
     setDia(DIA);
-    
+
   };
 
   const onAtendimento = () => {
 
-    console.log("\nLOG: ----------->CLICK BOTÃO ATENDIMENTO ---------> "+ATENDIMENTO);
+    console.log("\nLOG: ----------->CLICK BOTÃO ATENDIMENTO ---------> " + ATENDIMENTO);
 
-    if (ATENDIMENTO === INI_ATENDIMENTO){
+    if (ATENDIMENTO === INI_ATENDIMENTO) {
 
       ATENDIMENTO = FIM_ATENDIMENTO;
       ATENDIMENTO_STATUS = FIM_STATUS;
@@ -236,54 +266,54 @@ export const ButtonsControl = () => {
       ATIVAR_BATENDIMENTO = ENABLE_BOTTON;
       ATIVAR_BINTERVALO = DISABLE_BOTTON;
 
-    }else{
+    } else {
 
       ATIVAR_BDIA = ENABLE_BOTTON;
       ATIVAR_BATENDIMENTO = ENABLE_BOTTON;
       ATIVAR_BINTERVALO = ENABLE_BOTTON;
 
-      ATENDIMENTO = INI_ATENDIMENTO;  
+      ATENDIMENTO = INI_ATENDIMENTO;
       ATENDIMENTO_STATUS = INI_STATUS;
       ATENDIMENTO_APAR = APAR_INI;
 
     }
     setAtendimento(ATENDIMENTO);
-    
+
   };
 
   return (
     <Layout style={styles.container} level='3'>
 
-    <Card style={styles.card} status='danger'>
+      <Card style={styles.card} status='danger'>
 
-    <Text style={styles.text} status='warning'>Data: {currentDay} </Text>
-    <Text style={styles.text} status='danger'>Usuário: {currentUser} </Text>
+        <Text style={styles.text} status='warning'>Data: {currentDay} </Text>
+        <Text style={styles.text} status='danger'>Usuário: {currentUser} </Text>
 
-    <Text style={styles.text} status='primary'>Latitude: {currentLatitude} </Text>
-    <Text style={styles.text} status='primary'>Longitude: {currentLogitude} </Text>
-    <Text style={styles.text} status='success'>Horas Trabalhadas: {currentHorTrab}</Text>
-    <Text style={styles.text} status='success'>Horas Atendimento: {currentHorAten}</Text>
-    <Text style={styles.text} status='success'>Horas Deslocamento: {currentHorDesl}</Text>
+        <Text style={styles.text} status='primary'>Latitude: {currentLatitude} </Text>
+        <Text style={styles.text} status='primary'>Longitude: {currentLogitude} </Text>
+        <Text style={styles.text} status='success'>Horas Trabalhadas: {currentHorTrab}</Text>
+        <Text style={styles.text} status='success'>Horas Atendimento: {currentHorAten}</Text>
+        <Text style={styles.text} status='success'>Horas Deslocamento: {currentHorDesl}</Text>
 
-    <Text style={styles.text} status='info'>Inicio Dia: {currentIniDia} </Text>
-    <Text style={styles.text} status='info'>Fim Dia: {currentFimDia}</Text>
-    <Text style={styles.text} status='info'>Inicio Intervalo: {currentIniInt}</Text>
-    <Text style={styles.text} status='info'>Fim Intervalo: {currentFimInt}</Text>
-    
+        <Text style={styles.text} status='info'>Inicio Dia: {currentIniDia} </Text>
+        <Text style={styles.text} status='info'>Fim Dia: {currentFimDia}</Text>
+        <Text style={styles.text} status='info'>Inicio Intervalo: {currentIniInt}</Text>
+        <Text style={styles.text} status='info'>Fim Intervalo: {currentFimInt}</Text>
 
-      <Button style={styles.button} disabled={ATIVAR_BINTERVALO} status={INTERVALO_STATUS} appearance={INTERVALO_APAR} accessoryLeft={StarIcon} onPress={onIntervalo}>
-        {bIntervalor}
-    </Button>
 
-      <Button style={styles.button} disabled={ATIVAR_BATENDIMENTO} status={ATENDIMENTO_STATUS} appearance={ATENDIMENTO_APAR} accessoryLeft={StarIcon} onPress={onAtendimento}>
-        {bAtendimento}
-    </Button>
+        <Button style={styles.button} disabled={ATIVAR_BINTERVALO} status={INTERVALO_STATUS} appearance={INTERVALO_APAR} accessoryLeft={StarIcon} onPress={onIntervalo}>
+          {bIntervalor}
+        </Button>
 
-      <Button style={styles.button} disabled={ATIVAR_BDIA} status={DIA_STATUS} appearance={DIA_APAR} accessoryLeft={StarIcon} onPress={onDia}>
-        {bDia}
-    </Button>
+        <Button style={styles.button} disabled={ATIVAR_BATENDIMENTO} status={ATENDIMENTO_STATUS} appearance={ATENDIMENTO_APAR} accessoryLeft={StarIcon} onPress={onAtendimento}>
+          {bAtendimento}
+        </Button>
 
-    </Card>
+        <Button style={styles.button} disabled={ATIVAR_BDIA} status={DIA_STATUS} appearance={DIA_APAR} accessoryLeft={StarIcon} onPress={onDia}>
+          {bDia}
+        </Button>
+
+      </Card>
 
 
     </Layout>
@@ -307,7 +337,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   card: {
-    marginTop:0,
+    marginTop: 0,
     margin: 2,
   },
   text: {
